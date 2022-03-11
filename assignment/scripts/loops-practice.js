575304d87a24e342c81a001aef2dd8a07ad3eb01
Chad Rhiger <csrhig@gmail.com>
2022-03-11 17:25:28 -0600

    answered questions 2 through 3a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -35,20 +35,26 @@ for (let i=2; i<11; i+=2){
 console.log('STRETCH: countdown from 5 to 0');
 
 
-
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for(let i=0; i<stars.length; i++){
+  console.log(stars[i]);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+index = 0
+while(index<stars.length){
+  console.log(stars[index]);
+  index++;
+}
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
